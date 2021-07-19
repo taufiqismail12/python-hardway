cfeef9baac0debbcfc39244c5854712a07b47db8
taufiq ismail <taufiq.ismail@99.co>
2021-07-19 13:41:23 +0700

    update exercise 1 - 7

---
+++ ex7.py
@@ -0,0 +1,11 @@
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+print("." * 10)  # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
